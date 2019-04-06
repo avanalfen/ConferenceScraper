87ee75a8fa272f4ea61a67c2357fded24b53c03b
Austin Van Alfen <austin.van.alfen@gmail.com>
2019-04-05 21:00:32 -0600

    slight updates

--- Scraper.py
+++ Scraper.py
@@ -1,8 +1,8 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-year = "2013"
-month = "04"
+year = "2013" # you can choose any year
+month = "04" # months to choose are either '04' or '10'
 
 base = 'https://www.lds.org'
 url = 'https://www.lds.org/general-conference/' + year + '/' + month + '?lang=eng'
@@ -18,7 +18,7 @@ while len(talk_containers) > 0:
     first = talk_containers[0]
     titleDiv = first.find_all('div', class_='lumen-tile__title')[0].div
 
-    if titleDiv is None:
+    if titleDiv is None: // Crash fix
         talk_containers.pop(0)
         continue
 
@@ -27,30 +27,20 @@ while len(talk_containers) > 0:
     detailURL = base + first.a['href']
     imageURL = first.img['data-src']
 
-    # print(type(talk_containers))
-
-    #print(len(talk_containers))
-    print('Author: ' + author)
-    print('Title: ' + title)
-    print('Full Talk: ' + detailURL)
-    print('Image: ' + imageURL)
-
-    print(' ')
-
+    sleep(2) # don't shotgun the website
+    
     detailResponse = get(detailURL)
     detailSoup = BeautifulSoup(detailResponse.text, 'html.parser')
     fullTalk = detailSoup.find_all('div', class_ = 'body-block')[0].text
-
-    #print(fullTalk)
 
     filename = author + ' - ' + title + '.txt'
 
     with open(filename, 'a') as the_file:
         utf8 = fullTalk.encode("utf8")
         the_file.write(utf8)
-
-    #print(' ')
-
+    
+    sleep(2) // # don't shotgun the website
+    
     talk_containers.pop(0)
 
 print('Done')
